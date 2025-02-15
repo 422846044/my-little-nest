e3b78dc77707d4993af45ef8f0ad77f7a45f5b82
zhongyingjie <422846044@qq.com>
2025-02-15 11:55:43 +0800

    2025.2.25

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -6,7 +6,7 @@
   <div style="text-align: center;">
     <span>© 2024 淡风无痕. All rights reserved.</span>  
     <span class="links">  
-      <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2024022512号-1</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2024022512号-2</a>
     </span> 
   </div>
     
