0a7bd867793a4b76cb24ceb9be312bbf595c6e6a
zyj <422846044@qq.com>
2025-08-21 11:58:53 +0800

    2025.8.21

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref,inject, reactive, getCurrentInstance,onMounted } from 'vue';
+import { ref, inject, reactive, getCurrentInstance, onMounted } from 'vue';
 import { Search } from '@element-plus/icons-vue'
-import { djcTaskQuery,djcUpdateToken,djcTaskRetry } from '../api'
+import { djcTaskQuery, djcUpdateToken, djcTaskRetry } from '../api'
 const ruleFormRef = ref()
 
 var cookie = document.cookie;
@@ -9,40 +9,43 @@ var cookieList = cookie.split("; ");
 
 let emits = defineEmits(['changeInfoList'])
 
-let form = reactive({qqNum:''});
+let form = reactive({ qqNum: '' });
 
 for (let lengthKey in cookieList) {
   var keyValue = cookieList[lengthKey].split("=");
-  var key=keyValue[0];
-  var value=keyValue[1];
-  if(value!='undefinde'&&value!=''){
-    form.qqNum = value;
+  var key = keyValue[0];
+  if (key == 'qqNum') {
+    var value = keyValue[1];
+    if (value != 'undefinde' && value != '') {
+      form.qqNum = value;
+    }
   }
+
 }
 const { proxy } = getCurrentInstance()
 
 
-const queryLog = () =>{
-  djcTaskQuery({qqNum:form.qqNum})
-  .then(res=>{
-    if(res.data.success){
-      emits('changeInfoList',res.data.data)
-      document.cookie="qqNum="+form.qqNum;
-    }
-  });
+const queryLog = () => {
+  djcTaskQuery({ qqNum: form.qqNum })
+    .then(res => {
+      if (res.data.success) {
+        emits('changeInfoList', res.data.data)
+        document.cookie = "qqNum=" + form.qqNum;
+      }
+    });
 }
 
-const query = async (formEl)=>{
+const query = async (formEl) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
     if (valid) {
-        queryLog()
+      queryLog()
     }
-})
+  })
 }
 
-onMounted(()=>{
-  if(form.qqNum){
+onMounted(() => {
+  if (form.qqNum) {
     queryLog()
   }
 })
@@ -57,12 +60,12 @@ function updateToken() {
   }).then(({ value }) => {
     if (value != null && value.trim != '') {
       djcUpdateToken({ qqNum: form.qqNum, token: token })
-      .then(function (res) {
-        proxy.$message({
-          type: 'success',
-          message: `${err.data.msg}`,
+        .then(function (res) {
+          proxy.$message({
+            type: 'success',
+            message: `${err.data.msg}`,
+          })
         })
-      })
     } else {
       proxy.$message({
         type: 'warning',
@@ -82,12 +85,12 @@ function updateToken() {
 function retry() {
   if (props.qqNum.trim != '') {
     djcTaskRetry({ qqNum: form.qqNum })
-    .then(function (res) {
-      proxy.$message({
-        type:'success',
-        message:`${res.data.msg}`
+      .then(function (res) {
+        proxy.$message({
+          type: 'success',
+          message: `${res.data.msg}`
+        })
       })
-    })
   }
 }
 
@@ -110,26 +113,24 @@ const rules = reactive({
 <template>
   <div style="text-align: center;">
     <el-form label-position="left" :model="form" :rules="rules" ref="ruleFormRef" :inline="true">
-    <el-form-item label="QQ号" prop="qqNum">
-      <el-input  type="text" v-model.number="form.qqNum" autocomplete="off" placeholder="请输入查询的QQ号" :prefix-icon="Search" />
-    </el-form-item>
-    <el-form-item>
-      <el-button class="button" id="search" @click="query(ruleFormRef)">查询</el-button>
-    </el-form-item>
-    <el-form-item>
-      <el-button class="button" id="retry" @click="retry">补签</el-button>
-    </el-form-item>
-    <el-form-item>
-      <el-button class="button" id="update" @click="updateToken">更新token</el-button>
-    </el-form-item>
-    <el-form-item>
-      <el-button class="button" id="clear" @click="clearCooike">清除账号cookie记录</el-button>
-    </el-form-item>
-  </el-form>
+      <el-form-item label="QQ号" prop="qqNum">
+        <el-input type="text" v-model.number="form.qqNum" autocomplete="off" placeholder="请输入查询的QQ号"
+          :prefix-icon="Search" />
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button" id="search" @click="query(ruleFormRef)">查询</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button" id="retry" @click="retry">补签</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button" id="update" @click="updateToken">更新token</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button" id="clear" @click="clearCooike">清除账号cookie记录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
-  
-    
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
